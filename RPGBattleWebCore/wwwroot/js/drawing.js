2c0154de30f82d8223a3f855c13b229a2913a915
jonttanner <jonttanner@gmail.com>
2018-06-22 16:52:57 -0600

    decoding grid clicks and basic drawing.

---
+++ RPGBattleWebCore/wwwroot/js/drawing.js
@@ -0,0 +1,38 @@
+﻿class Point {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+}
+
+class Board {
+    constructor(width, height, widthRects, heightRects) {
+        this.widthRects = widthRects
+        this.heightRects = heightRects
+        this.width = width
+        this.height = height
+        this.rectWidth = width / widthRects
+        this.rectHeight = height / heightRects
+        this.cells = [widthRects][heightRects] //0,0 is top left
+    }
+}
+
+function drawLine(startPoint, endPoint, context) {
+    context.beginPath()  //TODO: test location for this
+    context.moveTo(startPoint.x, startPoint.y)
+    context.lineTo(endPoint.x, endPoint.y)
+    context.stroke()
+}
+
+function drawBoard(board, game) {
+    for (var i = 0; i <= board.widthRects; ++i) {
+        var startVert = new Point(i * 100, 0)
+        var endVert = new Point(i * 100, board.height)
+        drawLine(startVert, endVert, game.context)
+    }
+    for (var i = 0; i <= board.heightRects; ++i) {
+        var startHoriz = new Point(0, i * 100)
+        var endHoriz = new Point(board.width, i * 100)
+        drawLine(startHoriz, endHoriz, game.context)
+    }
+}
---
+++ RPGBattleWebCore/wwwroot/js/game.js
@@ -0,0 +1,27 @@
+﻿
+class Game {
+    constructor(canvas, context, widthRects, heightRects) {
+        this.canvas = canvas
+        //this.canvas2d = canvas.getContext("2d")
+        this.context = context
+        this.board = new Board(canvas.width, canvas.height, widthRects, heightRects)
+        this.onGameStart()
+    }
+
+    onGameStart() {
+        drawBoard(this.board, this)
+        this.setEventListeners()
+    }
+
+    setEventListeners() {
+        var board = this.board
+        this.canvas.addEventListener("click", function (sender) {
+            var xRect = Math.trunc(board.widthRects / (board.width / sender.offsetX))
+            var yRect = Math.trunc(board.heightRects / (board.height/ sender.offsetY))
+            //alert('x:' + xSquare + " y:" + ySquare)
+            var context = this.getContext("2d")
+            context.fillRect(xRect * board.rectWidth, yRect * board.rectHeight, board.rectWidth, board.rectHeight)
+        }, false)
+    }
+
+}
